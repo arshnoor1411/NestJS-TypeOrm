b34c4fec4c2b115cbd0c3fb0549760caa326c5dc
Arshnoor Singh <32287091+arshnoor1411@users.noreply.github.com>
2023-02-22 13:59:12 +0530

    enhance: kafka-added

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
- const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);
   //const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  await app.listen(5050,'0.0.0.0');
+  await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['localhost:9092'],
+      },
+    },
+  });
+
+  await app.listen(5050, '0.0.0.0');
 
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
